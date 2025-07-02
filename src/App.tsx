d2a65725c3cce1c7418f1defd2c18e64093f1098
Hariyono20 <bentohariyono00@gmail.com>
2025-07-02 21:22:42 +0700

    fix: ignore TS7016 errors for JSX imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
+// @ts-ignore
 import Navbar from "./components/Navbar";
+// @ts-ignore
 import Hero from "./sections/Hero";
+// @ts-ignore
 import About from "./sections/About";
+// @ts-ignore
 import Skills from "./sections/Skills";
+// @ts-ignore
 import Experience from "./sections/Experience";
+// @ts-ignore
 import Certificates from "./sections/Certificates";
+// @ts-ignore
 import Projects from "./sections/Projects";
+// @ts-ignore
 import Contact from "./sections/Contact";
+// @ts-ignore
 import Footer from "./sections/Footer";
+
 
 function App() {
   return (
